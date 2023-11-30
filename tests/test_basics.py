a8acb95d2e34bed5f3c7dd9d814915056c5a6d95
Joran R. Angevaare <joranangevaare@gmail.com>
2023-11-30 15:00:10 +0100

    Cache import routine (#21)
    * add caching routine
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add seperate test for loading
    
    * fix test to load the right module

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,11 +1,29 @@
 import unittest
 
 
+def test_install_on_fly():
+    import rpy_symmetry as rsym
+
+    rsym.get_module('symmetry')
+
+
 def test_minimal():
     import rpy_symmetry as rsym
 
     pval = rsym.p_symmetry([1, 2, 3])
     assert pval
+
+
+def test_wo_cache():
+    import rpy_symmetry as rsym
+
+    assert rsym.rpy_symmetry._imported_package
+    rsym.rpy_symmetry.ALLOW_CACHING = False
+    rsym.rpy_symmetry._imported_package = {}
+    pval = rsym.p_symmetry([1, 2, 3])
+
+    assert pval
+    assert rsym.rpy_symmetry._imported_package == {}
 
 
 class RaiseMod(unittest.TestCase):
